689ff9bb99104de4f61d1959fbaf1eee42a80d47
Wasiul Islam Sakif <sakif049@gmail.com>
2024-11-07 16:10:31 +0600

    fix: logo and title

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Zimmer550i',
-      scrollBehavior: const MaterialScrollBehavior()
-          .copyWith(dragDevices: {PointerDeviceKind.mouse}),
+      scrollBehavior: const MaterialScrollBehavior().copyWith(dragDevices: {
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.touch,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.invertedStylus,
+        PointerDeviceKind.trackpad,
+        PointerDeviceKind.unknown,
+      }),
       theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
